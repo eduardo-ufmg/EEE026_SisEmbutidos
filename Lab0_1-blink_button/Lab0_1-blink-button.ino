8f31b674948f556d56b7d54540034c558a4fdf4b
Eduardo Henrique Basilio de Carvalho <eduardohbc@ufmg.br>
2024-10-23 19:21:52 +0000

    change code to new requirements

--- Lab0_1-blink_button/Lab0_1-blink-button.ino
+++ Lab0_1-blink_button/Lab0_1-blink-button.ino
@@ -2,33 +2,27 @@
 
 const int ledPin = 2;
 const int buttonPin = 3;
-const int period = 1000;
+const int period = 2000;
 const int blinkInterval = period / 2;
 const int debounceTime = 100;
+const int ledAckHighDuration = 250;
 
 bool debounce(int pin, int interval, int stateToCheck);
+void waitForButtonTobePressedandReleased(int buttonPin, int debounceTime, int pressedState = LOW);
+void blinkLed(int ledPin, int interval);
+void ackLed(int ledPin, int duration);
 
 void setup()
 {
   pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT_PULLUP); // no need for external pull-up resistor
-
-  while (not debounce(buttonPin, debounceTime, LOW)) {
-    // Wait for the button to be pressed
-  }
-
-  while (not debounce(buttonPin, debounceTime, HIGH)) {
-    // Wait for the button to be released
-  }
-
 }
 
 void loop()
 {
-  digitalWrite(ledPin, HIGH);
-  delay(blinkInterval);
-  digitalWrite(ledPin, LOW);
-  delay(blinkInterval);
+  waitForButtonTobePressedandReleased(buttonPin, debounceTime);
+  blinkLed(ledPin, blinkInterval);
+  ackLed(ledPin, ledAckHighDuration);
 }
 
 bool debounce(int pin, int interval, int stateToCheck)
@@ -40,4 +34,36 @@ bool debounce(int pin, int interval, int stateToCheck)
     }
   }
   return false;
+}
+
+void waitForButtonTobePressedandReleased(int buttonPin, int debounceTime, int pressedState)
+{
+  int releasedState = not pressedState;
+
+  while (!debounce(buttonPin, debounceTime, pressedState)) {
+    // wait for the button to be pressed
+  }
+
+  while (!debounce(buttonPin, debounceTime, releasedState)) {
+    // wait for the button to be released
+  }
+}
+
+void blinkLed(int ledPin, int interval)
+{
+  int ledState = digitalRead(ledPin);
+
+  digitalWrite(ledPin, not ledState);
+  delay(interval);
+  digitalWrite(ledPin, ledState);
+  delay(interval);
+}
+
+void ackLed(int ledPin, int duration)
+{
+  int ledState = digitalRead(ledPin);
+
+  digitalWrite(ledPin, not ledState);
+  delay(duration);
+  digitalWrite(ledPin, ledState);
 }
